d78a45159fde91a5b7114eeb69923cc5afbe4cff
AritraLikhan <aritralikhan2001@gmail.com>
2024-11-10 19:11:10 +0600

    App delegate and imagepicker implemented

---
+++ Nostalgia/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Nostalgia
+//
+//  Created by Tanmoy Biswas on 13/11/24.
+//
+
+import Foundation
---
+++ Nostalgia/ImagePicker.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import PhotosUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            if let result = results.first, result.itemProvider.canLoadObject(ofClass: UIImage.self) {
+                result.itemProvider.loadObject(ofClass: UIImage.self) { object, _ in
+                    self.parent.image = object as? UIImage
+                }
+            }
+        }
+    }
+}
