18f3a598cab0aa464129ef9b8392c99a4a2317bd
AritraLikhan <aritralikhan2001@gmail.com>
2024-11-12 12:07:39 +0600

    Date format fixed

---
+++ Nostalgia/DateFormatterExtension.swift
@@ -0,0 +1,8 @@
+import Foundation
+extension DateFormatter {
+    static var shortDate: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
+    }
+}
